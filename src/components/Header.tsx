8b4d9c8a8fe807687419c0c4a11c027ee7dd2f52
LesyaSD <olesamin04@gmail.com>
2025-11-01 21:51:51 +0300

    fix route img

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,24 @@
 import styles from '../ui/Header.module.scss';
+import ring from '../../images/bell.png'
+import heart from '../../images/heart.png'
+import logo from '../../images/Logo.svg'
+
+
+
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.headerInner}>
           <a href="" className={styles.headerLogo}>
-            <img src="./images/logo.svg" alt="" />
+            <img src={logo} alt="" />
           </a>
           <div className={styles.headerNote}>
             <button className={styles.hiddenMobile} aria-label="Избранное">
-              <img src="./images/heart.png" width={24} alt="" />
+              <img src={heart} width={24} alt="" />
             </button>
             <button className={styles.hiddenMobile}>
-              <img src="./images/bell.png" width={24} alt="" />
+              <img src={ring} width={24} alt="" />
             </button>
             <div className={styles.headerUser}>
               <img src="./images/cat.jpg" width={40} alt="" />
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { fetchUsers } from "../api/usersApi";
 import styles from "../ui/UserCard.module.scss";
+import avatar from '../../images/cat.jpg'
 
 export default function UserCard() {
   interface User {
@@ -30,7 +31,7 @@ export default function UserCard() {
       <div className={styles.cardInfo}>
         {users.map((user) => (
           <div key={user.id} className={styles.cardItem}>
-            <img src={"./images/cat.jpg"} alt={user.name} width={120} />
+            <img src={avatar} alt={user.name} width={120} />
             <div className={styles.cardText}>
               <h5 className={styles.cardUserName}>{user.name}</h5>
               <p>{user.company.name}</p>
